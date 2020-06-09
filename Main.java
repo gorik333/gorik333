aca86b6971ac8338b1ca4736810a1145dce30040
gorik333 <50589173+gorik333@users.noreply.github.com>
2020-06-09 19:09:27 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,71 +1,72 @@
+import java.util.Arrays;
 /**
- * Завдання: Модифікувати лабораторну роботу №3 наступним чином: для літер, слів, речень, розділових знаків та тексту
- * створити окремі класи. Слово повинно складатися з масиву літер, речення з масиву слів та розділових знаків,
- * текст з масиву речень. Замінити послідовність табуляцій та пробілів одним пробілом.
+ * C13 = 10 (9318 mod 13 = 10)
  *
- * Створити клас, який складається з виконавчого методу, що виконує описану дію з лабораторної роботи №3
- * (з кожного речення заданого тексту видалити підрядок найбільшої довжини, що починається та закінчується заданими
- * літерами), але в якості типів використовує створені класи.
+ * Завдання: Завантажити фургон певного обсягу вантажем на певну суму з різних сортів кави, що можуть перебувати у
+ * різних фізичних станах (зерно, мелена, розчинна в банках і пакетиках). Ураховувати обсяг кава разом з упаковкою.
+ * Провести сортування товарів на основі співвідношення ціни й ваги. Знайти товар у фургоні, що відповідає заданому
+ * діапазону якості кави.
  *
- * Програма містить шість класів:
- *  - Клас Letter;
- *  - Клас Punctuation;
- *  - Клас Word;
- *  - Клас Sentence;
- *  - Клас Text;
- *  - Клас Main (із виконавчим методом).
- *
- * У виконавчому методі ми створюємо об’єкт класу Text, за допомогою конструктора Text(String s),
- * де параметр «s»-наш заданий текст, потім створюємо два об’єкти класу Letter, за допомогою конструктора Letter(char a),
- * параметр «a» задає поле «letter» для кожного об’єкту. Далі за допомогою метода removeOfString(Text t, Letter a, Letter b)
- * виконуємо дію з лабораторно роботи №3. Відредагований текст буде виведено на екран.
- *
+ * Створено клас кави, що складається  з 4 полів: стан кави, запокованість, вага та ціна, і 7 нащадків цього класу:
+ * Starbucks, CostaCoffee, DunkinDonuts, Nescafe, FigaroCoffee, Lofbergs, Lavazza.
+ * Провдиться оцінка якості кави, як співвідношення ціни до маси. Результати виконання виводяться на екран у
+ * відсортованому вигляді.
  *
  * @author Куцурайс Георгій.
  * @version 1.0 30 May 2020.
  *
  */
+
 public class Main {
-    /**
-     * Method that removes from the specified string specified substring.
-     *
-     * @param t associated with an object of class Text.
-     * @param a indicates the beginning of the substring.
-     * @param b indicates the end of the substring.
-     */
-    private static void removeOfString(Text t, Letter a, Letter b) {
-        boolean k = true;
-        StringBuffer s = new StringBuffer();
-        for (int i = 0; i < t.getSentence().length; i++) {
-            int a1 = t.getSentence()[i].getSent().indexOf(a.getLetter());
-            int b1 = t.getSentence()[i].getSent().lastIndexOf(b.getLetter());
-            if (a1 != -1 && b1 != -1) {
-                s.append(t.getSentence()[i].getSent().substring(0, a1)
-                        + t.getSentence()[i].getSent().substring(b1 + 1));
-                k = false;
-            } else {
-                s.append(t.getSentence()[i].getSent());
-            }
-        }
-        if (k) {
-            System.out.println("В даному рядку не знайдено заданих символів.");
-        } else {
-            Text t1 = new Text(s.toString());
-            System.out.println("Оригінальний текст:" + "\n" + t.getTxt());
-            System.out.println("Видозмінений текст:");
-            System.out.println(t1.getTxt());
-        }
-    }
 
+    private static double func(double d) {
+        d=d*100;
+        int i = (int)Math.round(d);
+        d=(double)i/100;
+        return d;
+    }
     public static void main(String[] args) {
-        String s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " +
-                "labore et dolore magna aliqua? Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
-                "nisi ut          aliquip ex ea commodo consequat! Duis aute irure dolor in reprehenderit in voluptate velit " +
-                "esse cillum dolore eu fugiat nulla pariatur.";
-        Text b = new Text(s);
-        Letter a = new Letter('m');
-        Letter k = new Letter('a');
-        removeOfString(b, a, k);
-    }
 
+        Coffee[] array = new Coffee[] {
+                new Starbucks("Мелена","Банка",11200,2500),
+                new CostaCoffee("Зерно","Пакет",270,1000),
+                new DunkinDonuts("Розчинна","Банка",100,95),
+                new Nescafe("Розчинна","Пакет",50,120),
+                new FigaroCoffee("Зерно", "Банка", 300, 80),
+                new Lofbergs("Мелена", "Пакет", 100, 1150),
+                new Lavazza("Мелена","Пакет",60,250)
+        };
+        int sum=0;
+        System.out.print(" Загальна маса усіх марок кави - ");
+        for (Coffee anArray : array) {
+            sum = sum + anArray.GetWeight();
+        }
+        System.out.println(sum+" гр");
+
+        double[] quality = new double[array.length];
+        for (int i = 0; i < array.length; i++) {
+            quality[i] = array[i].GetPrice()/array[i].GetWeight();
+        }
+
+        Arrays.sort(quality);
+
+        System.out.println(new String(new char[40]).replace("\0", "—"));
+        System.out.printf("%-17s%-50s%-1s\n", "⎜", "Якість (співвідношення ціни до маси)","⎜");
+        System.out.println(new String(new char[40]).replace("\0", "—"));
+        System.out.printf("%-3s%-3s%-6s%-14s%-6s%-10s%-3s%-13s%-3s%-8s%s\n", "⎜", "#","⎜","Назва кави","⎜", "Стан","⎜", "Запакований","⎜", "Якість","⎜");
+        System.out.println(new String(new char[40]).replace("\0", "—"));
+        for (int i=quality.length-1;i>=0;i--) {
+            for (Coffee anArray : array) {
+                if (quality[i] == anArray.GetPrice() / anArray.GetWeight()) {
+                    System.out.print("⎜  "+(quality.length - i) + "  ");
+                    anArray.GetPackCon();
+                    System.out.println(func(quality[i]) + "   ⎜");
+                }
+            }
+            System.out.println(new String(new char[40]).replace("\0", "—"));
+        }
+
+
+    }
 }
+
